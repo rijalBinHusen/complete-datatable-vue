fc7bfdf0aaf01f9b96bf7e62629ce3e6b600bfca
rijalBinHusen <jeryzal333@gmail.com>
2021-06-28 10:08:57 +0700

    create component and dummy data

---
+++ script.js
@@ -0,0 +1,26 @@
+new Vue({
+	el: "#utama",
+    data: {
+        data1:  dummyData()
+    }
+})
+
+function generator (length) {
+    let str = "qwertyuiopasdfghjkllzxcvbnm1234567890QWERYUIOPASDFGHJKLZXCVBNM"
+    let result = "";
+
+    for (i = 1; i <= Number(length); i ++) {
+        result += str[Math.round(Math.random() * str.length) ]
+    }
+    return result
+}
+
+function dummyData () {
+    let result = []
+
+    for (let i = 0; i < 150; i ++) {
+        result.push({id: i, dat1: generator(6), dat2: generator(9), dat3: generator(5)})
+    }
+
+    return result
+}
