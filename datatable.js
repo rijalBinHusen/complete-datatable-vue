78f117e5075e4b7dca5165f0418bac49702b48ca
rijalBinHusen <jeryzal333@gmail.com>
2021-06-28 13:14:35 +0700

    success pagination button with highlights @current page

--- datatable.js
+++ datatable.js
@@ -2,23 +2,27 @@ Vue.component("datatable", {
 	props: ["datanya"],
     data () {
         return {
-            currentPage: 0,
+            startRow: 0,
             lengthRow: 5,
-            nowData: null
+            nowData: null,
+            currentPage: 0
         }
     },
     computed: {
         paginate () {
-            return this.datanya.slice(this.currentPage, Number(this.currentPage)+Number(this.lengthRow))
-            // console.log(this.datanya.slice(this.currentPage, this.lengthRow))
+            return this.datanya.slice(this.startRow, this.startRow+Number(this.lengthRow))
+            // console.log(this.datanya.slice(this.startRow, this.lengthRow))
         },
         totalPage () {
-           return this.datanya.length / this.lengthRow
+            return this.startRow == 0 || this.startRow == 1 ? [1,2,3] : [this.currentPage-1, this.currentPage, this.currentPage+1]
+            //return  [1,2,3,4,5,6]
+        //    this.datanya.length / this.lengthRow
         }
     },
     methods: {
         toThePage(num) {
-            this.currentPage = (num-1)*this.lengthRow
+            this.startRow = (num-1)*this.lengthRow
+            this.currentPage = num
         }
     },
     template: `
@@ -26,6 +30,7 @@ Vue.component("datatable", {
     <!-- pagination length -->
 
         <span>
+        Show entries
         <select v-model="lengthRow">
             <option value="5">5</option>
             <option value="10">10</option>
@@ -51,7 +56,7 @@ Vue.component("datatable", {
             </thead>
             <tbody>
             <tr v-for="(r, index) in paginate">
-                <th scope="row">{{index+currentPage+1}}</th>
+                <th scope="row">{{index+startRow+1}}</th>
                 <td>{{r.dat1}}</td>
                 <td>{{r.dat2}}</td>
                 <td>{{r.dat3}}</td>
@@ -67,14 +72,17 @@ Vue.component("datatable", {
                 <li class="page-item disabled">
                     <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
                 </li>
-                <!--li class="page-item"><a class="page-link" href="#" @click="toThePage(0)">1</a></li>
-                <li class="page-item"><a class="page-link" href="#" @click="toThePage(2)">2</a></li>
-                <li class="page-item"><a class="page-link" href="#" @click="toThePage(3)">3</a></li!-->
-                <li v-for="p in totalPage">
-                    <a class="page-link" @click="toThePage(p)" :value="p" href="#">{{p}}</a>
+
+                <!--li :class="{page-item, 'active':currentPage == p}"><a class="page-link" href="#" @click="toThePage(1)">1</a></li>
+                <li class="page-item"><a class="page-link " href="#" @click="toThePage(2)">2</a></li>
+                <li class="page-item"><a class="page-link" href="#" @click="toThePage(3)">3</a></li-->
+
+                <li :class="['page-item', currentPage == p ? 'active' : '' ]" v-for="p in totalPage">
+                    <a class="page-link" @click="toThePage(p)" href="#">{{p}}</a>
                 </li>
+
                 <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
+                    <a class="page-link active" href="#">Next</a>
                 </li>
                 </ul>
             </nav>
